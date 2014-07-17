8734dc87bcde1b899b14d1fadd57141d75d3914c
Louis Larpin <louis.larpin@gmail.com>
2014-07-17 20:38:29 +0200

    Fix whenever capistrano role

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,6 +47,9 @@ set :sidekiq_config, File.join(shared_path, 'sidekiq.yml')
 # Default value for :sidekiq_require is :nil
 set :sidekiq_require, File.join(release_path, 'lib', 'koin.rb')
 
+# Default value for :whenever_roles is :db
+set :whenever_roles, :app
+
 namespace :deploy do
   task :restart_nginx do
     on roles(:web), wait: 3 do
